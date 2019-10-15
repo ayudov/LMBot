a887e07d85a2f41cb274f37addfcb1a74e70fa5c
Andrey Yudov <yudovv@gmail.com>
2019-10-15 17:59:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-TOKEN = '611203851:AAGKRhdLhseIK4Xhu-Ftyp_VUNXXclfudH0'
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+print('your TOKEN:' + TOKEN)
 APP_NAME = 'leroymerlintgbot'
